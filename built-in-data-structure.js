a8377a0cab6ee85881d81c6e19405896bc1ff469
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-19 11:00:54 +0700

    array, objec, set

---
+++ built-in-data-structure.js
@@ -0,0 +1,59 @@
+/**
+ * 1. ARRAY
+ */
+const arr = [1, 2, 3, "Thanh Dat"];
+
+// add
+arr.push(4);
+arr.unshift(0);
+// remove
+arr.pop();
+arr.shift();
+
+for (const item of arr) {
+  //   console.log(item)
+}
+// map, filter, forEach, reduce, concat, slice, splice
+
+// -----------------------------------------------------
+
+/**
+ * 2. OBJECT
+ */
+
+const obj = {
+  name: "Thanh Dat",
+  age: 36,
+  "key-tree": true,
+  sayMyName: function () {
+    console.log(this.name);
+  },
+};
+obj.hobby = "football";
+delete obj.hobby;
+
+// console.log(obj.name);
+// console.log(obj["age"]);
+// console.log(obj["key-tree"]);
+// console.log(obj);
+// obj.sayMyName();
+
+// Object.keys / .values /.entries
+
+// -----------------------------------------------------
+
+/**
+ * 3. SET
+ */
+const set = new Set([1, 2, 3]);
+set.add("Thanh Dat");
+set.add("Thanh Dat"); // ignore for duplicate value
+set.delete(2);
+// console.log("has method", set.has(1));
+// console.log("size attribute", set.size);
+
+set.clear(); // remove all item
+
+for (const item of set) {
+  //   console.log("loop", item);
+}
