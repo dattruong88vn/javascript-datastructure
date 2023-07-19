89a75fa33d907f7c63af6a05581256dd79dc3590
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-07-19 11:27:45 +0700

    Binary Search Tree: tree traversal (DFS - Depth First Search)

--- Tree/BinaryTree.js
+++ Tree/BinaryTree.js
@@ -51,16 +51,47 @@ class BinarySearchTree {
       return this.search(root.right, value);
     }
   }
+
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+  }
+
+  inOrder(root) {
+    if (root) {
+      this.inOrder(root.left);
+      console.log(root.value);
+      this.inOrder(root.right);
+    }
+  }
+
+  postOrder(root) {
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
 
-console.log(bst.isEmpty());
+// console.log(bst.isEmpty());
+
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+bst.insert(3);
+bst.insert(7);
 
-console.log(bst.search(bst.root, 10));
-console.log(bst.search(bst.root, 5));
-console.log(bst.search(bst.root, 15));
-console.log(bst.search(bst.root, 20));
+// console.log(bst.search(bst.root, 10));
+// console.log(bst.search(bst.root, 5));
+// console.log(bst.search(bst.root, 15));
+// console.log(bst.search(bst.root, 20));
+
+// bst.preOrder(bst.root);
+// bst.inOrder(bst.root);
+bst.postOrder(bst.root);
